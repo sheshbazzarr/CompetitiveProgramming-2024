04f95162f529754cde4cc52bbe5a41097105d45a
Daniel Demerw <99654542+danieldemerw@users.noreply.github.com>
2024-02-07 04:27:24 -0500

    Time: 1980 ms (13.04%), Space: 25.4 MB (45.13%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,26 +1,22 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        
-        len_i=len(nums)
-        if len_i==1:
+        len_i = len(nums)
+        if len_i <= 1:
             return nums
-        mid=int(len_i/2)
-        nums1=self.sortArray(nums[0:mid])
-        nums2=self.sortArray(nums[mid:])
         
-        return self.Merge(nums1,nums2) 
+        mid = len_i // 2
+        nums1 = self.sortArray(nums[:mid])  # Corrected the reference to self.sortArray
+        nums2 = self.sortArray(nums[mid:])  # Corrected the reference to self.sortArray
+        
+        return self.Merge(nums1, nums2)  # Corrected the reference to self.Merge
     
-    def Merge(self,A,B):
-        newarray=[]
-        while len(A)>0 and len(B)>0:
-            if A[0]<B[0]:
-                newarray.append(A[0])
-                A.pop(0)
+    def Merge(self, A, B):  # Added self parameter to Merge method
+        newarray = []
+        while A and B:  # Simplified condition
+            if A[0] < B[0]:
+                newarray.append(A.pop(0))  # Using pop() to remove and get the first element
             else:
-                newarray.append(B[0])
-                B.pop(0)
-        if len(A)==0:
-            newarray+=B
-        if len(B)==0:
-            newarray+=A
+                newarray.append(B.pop(0))  # Using pop() to remove and get the first element
+        newarray.extend(A)  # Appending remaining elements in A
+        newarray.extend(B)  # Appending remaining elements in B
         return newarray
