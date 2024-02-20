32abb01eb0599391e00d09c7be47561f6d5aee63
Daniel Demerw <99654542+danieldemerw@users.noreply.github.com>
2024-02-20 09:24:39 -0500

    Time: 95 ms (56.66%), Space: 16.7 MB (74.41%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,19 +1,18 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        hasho = {}
-        most_frequent = 0
-        start = 0
-        max_len = 0
+        n = len(s)
+        max_length = 0
+        count = defaultdict(int)
 
-        for end, char in enumerate(s):
-            hasho[char] = hasho.get(char, 0) + 1
+        left = 0
+        for right in range(n):
+            count[s[right]] += 1
+            max_count = max(count.values())
 
-            most_frequent = max(most_frequent, hasho[char])
-            # (end - start + 1) --> window size
-            if (end - start + 1) - most_frequent > k:
-                hasho[s[start]] -= 1
-                start += 1
+            if right - left + 1 - max_count > k:
+                count[s[left]] -= 1
+                left += 1
 
-            max_len = max(max_len, end-start+1)
+            max_length = max(max_length, right - left + 1)
 
-        return max_len
+        return max_length
