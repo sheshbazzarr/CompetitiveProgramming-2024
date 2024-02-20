447f9ee00c60888ca4591d7d8b07d0e0833ecd19
Daniel Demerw <99654542+danieldemerw@users.noreply.github.com>
2024-02-20 08:41:41 -0500

    Time: 78 ms (84.79%), Space: 16.7 MB (74.41%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,23 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l = 0
-        c_frequency = {}
-        longest_str_len = 0
-        for r in range(len(s)):
+        hasho = {}
+        most_frequent = 0
+        start = 0
+        max_len = 0
 
-            if not s[r] in c_frequency:
-                c_frequency[s[r]] = 0
-            c_frequency[s[r]] += 1
+        for end, char in enumerate(s):
 
-            # Replacements cost = cells count between left and right - highest frequency
-            cells_count = r - l + 1
-            if cells_count - max(c_frequency.values()) <= k:
-                longest_str_len = max(longest_str_len, cells_count)
+            hasho[char] = hasho.get(char, 0) + 1
 
-            else:
-                c_frequency[s[l]] -= 1
-                if not c_frequency[s[l]]:
-                    c_frequency.pop(s[l])
-                l += 1
+            most_frequent = max(most_frequent, hasho[char])
+            # (end - start + 1) --> window size
+            if (end - start + 1) - most_frequent > k:
+                hasho[s[start]] -= 1
+                start += 1
 
-        return longest_str_len
+            max_len = max(max_len, end-start+1)
+
+        return max_len
