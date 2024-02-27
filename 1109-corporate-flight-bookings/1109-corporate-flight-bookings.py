b27cc31ac0a01eac1c15b677992440e967c5f787
Daniel Demerw <99654542+danieldemerw@users.noreply.github.com>
2024-02-27 05:49:04 -0500

    Time: 646 ms (96.14%), Space: 30.6 MB (88.57%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,16 +1,10 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
         
-        prefix_sum = [0] * (n + 1)
-        for firsti, lasti, seatsi in bookings:
-            prefix_sum[firsti] += seatsi
-            if lasti + 1 <= n:
-                prefix_sum[lasti + 1] -= seatsi
+       
+        arr = [0]*(n+1)
+        for lv, ar, seats in bookings:
+            arr[lv-1]+= seats
+            arr[ar]-= seats
 
-        answer = [0] * n
-        running_sum = 0
-        for i in range(n):
-            running_sum += prefix_sum[i + 1]
-            answer[i] = running_sum
-
-        return answer
+        return list(accumulate(arr[:-1]))
