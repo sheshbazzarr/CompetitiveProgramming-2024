4fafb18e88a88344fece8f63376d6684c2203e39
Daniel Demerw <99654542+danieldemerw@users.noreply.github.com>
2024-03-12 03:38:37 -0500

    Time: 52 ms (73.54%), Space: 16.7 MB (55.53%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,28 +5,32 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummynode=ListNode(0)
-        tailpointer=dummynode
+        dummyNode=ListNode(0)
+        currentpointer=dummyNode
         reminder=0
         while l1 or l2 or reminder:
-            value1=l1.val if l1 else 0
-            value2=l2.val if l2 else 0
-            
-            add=value1+value2 +reminder
-            reminder =add//10
-            add=add%10
-            newNode=ListNode(add)
-            tailpointer.next=newNode
-            tailpointer=tailpointer.next
-            
+            link1=l1.val if l1 else 0
+            link2=l2.val if l2 else 0
+            sumlink=link1+link2+reminder
+           
+            reminder=sumlink//10
+            sumlink=sumlink%10
+            newnode=ListNode(sumlink)
+            currentpointer.next =newnode
+            currentpointer=currentpointer.next
             
             l1=l1.next if l1 else None
             l2=l2.next if l2 else None
             
-            
-        result=dummynode.next
-        dummynode.next=None
+        result =dummyNode.next
+        dummyNode.next =None
         return result
+            
+            
+
+            
+          
+            
             
             
         
