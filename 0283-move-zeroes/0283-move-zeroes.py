ee26c1840505cbb5d4704204d14f96c4c7ab8e3f
Daniel Demerw <99654542+danieldemerw@users.noreply.github.com>
2024-03-20 15:06:14 -0500

    Time: 138 ms (25.29%), Space: 18.3 MB (6.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l=0
-        for r in range(len(nums)):
-            if nums[r]!=0:
-                nums[l],nums[r]=nums[r],nums[l]
-                l+=1
-        return nums
+        left=0
+        right=len(nums)-1
+        while right >left:
+            if nums[left]==0:
+                nums.pop(left)
+                nums.append(0)
+                right-=1
+            else:
+                left+=1
+      
+        
+                
+        
