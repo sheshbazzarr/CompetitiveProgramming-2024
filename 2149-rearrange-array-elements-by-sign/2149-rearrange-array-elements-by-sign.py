c894173f1f5119d3a4815996785c4280afee4b37
Daniel Demerw <99654542+danieldemerw@users.noreply.github.com>
2024-03-22 12:49:32 -0500

    Time: 995 ms (86.03%), Space: 48.2 MB (12.04%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,27 +1,15 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
 #         this is not effective solution thou
-        positive = deque()
-        negative = deque()
-        result = []
-
+        post=[]
+        negat=[]
+        result=[]
         for num in nums:
-            if num > 0:
-                positive.append(num)
+            if num>0:
+                post.append(num)
             else:
-                negative.append(num)
-
-        while positive or negative:
-            if positive:
-                result.append(positive.popleft())
-            if negative:
-                result.append(negative.popleft())
-
-            if not positive:
-                result.extend(negative)
-                break
-            if not negative:
-                result.extend(positive)
-                break
-
+                negat.append(num)
+        for i in range(len(post)):
+            result.append(post[i])
+            result.append(negat[i])
         return result
