b821de06676f5c9aa9a8920890779b2f8f387fe5
Daniel Demerw <99654542+danieldemerw@users.noreply.github.com>
2024-03-23 02:58:04 -0500

    Time: 166 ms (5.98%), Space: 17.8 MB (95.38%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.py
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -1,16 +1,11 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        count=0
-        for i in range(len(nums)-1):
-            if nums[i]>nums[i+1]:
-                count+=1
-                if i>0 and nums[i+1]<nums[i-1]:
-                    nums[i+1]=nums[i]
-                else:
-                    nums[i]=nums[i+1]
-        return count<=1
-                
-                    
-                
+        count = 0
+        for num in range(1, len(nums)):
+            if nums[num] < nums[num-1]:
+                if num > 1 and nums[num-2] > nums[num]:
+                    nums[num] = nums[num-1]
+                count += 1
+        return count <= 1
                     
                 
