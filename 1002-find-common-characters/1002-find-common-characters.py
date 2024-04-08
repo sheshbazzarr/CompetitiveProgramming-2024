e632ddd813a14b87e0fc01e5ccf5cf103563fb65
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-08 07:24:27 -0500

    Time: 52 ms (44.86%), Space: 16.7 MB (74.05%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -4,12 +4,16 @@ class Solution:
         for s in words:
             current_count=[0]*26
             for c in s:
-                current_count[ord(c)-ord('a')]+=1
+                c=ord(c)
+                a=ord('a')
+                current_count[c-a]+=1
             for c in range(26):
-                random_count[c]=min(random_count[c],current_count[c])
+                total_count_bit=random_count[c]
+                current_count_bit=current_count[c]
+                random_count[c]=min(total_count_bit,current_count_bit)
                 
         result=[]
         for c in range(26):
-            for _ in range(random_count[c]):
+            for x in range(random_count[c]):
                 result.append(chr(c+ord("a")))
         return result
