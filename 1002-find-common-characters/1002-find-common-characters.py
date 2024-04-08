7a7e32a656182f3048129fe41783991c0a59fd77
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-08 06:48:39 -0500

    Time: 48 ms (66.34%), Space: 16.7 MB (74.05%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -0,0 +1,15 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        random_count=[1000000]*26
+        for s in words:
+            current_count=[0]*26
+            for c in s:
+                current_count[ord(c)-ord('a')]+=1
+            for c in range(26):
+                random_count[c]=min(random_count[c],current_count[c])
+                
+        result=[]
+        for c in range(26):
+            for _ in range(random_count[c]):
+                result.append(chr(c+ord("a")))
+        return result
