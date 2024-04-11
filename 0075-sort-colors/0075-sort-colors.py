2cc134fbcb292fa03ba398dcb8b09ddc9a343433
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-11 08:49:40 -0500

    Time: 41 ms (29.14%), Space: 16.6 MB (42.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,6 +1,5 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-       
         count=[0,0,0]
         for num in nums:
             count[num]+=1
@@ -9,6 +8,7 @@ class Solution:
             for x in range(freq):
                 nums[index]=color
                 index+=1
+    
                 
         
         
