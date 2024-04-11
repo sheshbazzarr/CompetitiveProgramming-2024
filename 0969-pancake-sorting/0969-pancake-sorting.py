9293fb127c94decc038c211659ecb23f99a68274
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-11 12:14:44 -0500

    Time: 41 ms (66.55%), Space: 16.7 MB (5.49%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -1,16 +1,16 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
         n = len(arr)
-        res = []
-        for i in range(n):
-            cur_max = max(arr[0:n-i])
-            j = 0
-            while arr[j] != cur_max:
-                j += 1
-            # should reverse j+1 elements
-            arr[:j+1] = reversed(arr[:j+1])
-            res.append(j+1)
-            # reverse all
-            arr[:n-i] = reversed(arr[:n-i])
-            res.append(n-i)
-        return res
+        s = sorted(arr)
+        if arr == s:
+            return []
+
+        flips = []
+        for pancake in range(n):
+            m = arr.index(max(arr[:n-pancake]))
+            if m != n-pancake-1:
+                arr[:m+1] = arr[:m+1][::-1]
+                arr[:n-pancake] = arr[:n-pancake][::-1]
+                flips.extend([m+1, n-pancake])
+        
+        return flips
