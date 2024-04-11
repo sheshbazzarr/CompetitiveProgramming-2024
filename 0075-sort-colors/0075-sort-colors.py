cc692198d553cbf03213c6c97470e2d22ea8c50b
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-11 11:51:22 -0500

    Time: 40 ms (36.53%), Space: 16.5 MB (89.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,5 +1,13 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
+        count=[0,0,0]
+        for num in nums:
+            count[num]+=1
+        index=0
+        for color,freq in enumerate(count):
+            for z in range(freq):
+                nums[index]=color
+                index+=1
+                
         
         
