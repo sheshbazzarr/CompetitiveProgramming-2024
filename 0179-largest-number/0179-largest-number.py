ba277001fc732fa1375a62ad66b8222d70592ea9
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-12 05:21:27 -0500

    Time: 50 ms (19.63%), Space: 16.6 MB (28.27%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,25 +1,20 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        lst = []
-        for ele in nums:
-            lst += [str(ele)]
-        n = len(lst)
-        for i in range(n):
-            for j in range(i+1 , n):
+        str_list=[]
+        for ent in nums:
+            str_list+=[str(ent)]
+        L=len(str_list)
+        for sLw in range(L):
+            for fAs in range(sLw+1,L):
+                ascend=str_list[sLw]+str_list[fAs]
+                descend=str_list[fAs]+str_list[sLw]
                 
-                if str(lst[i]) + str(lst[j]) > str(lst[j]) + str(lst[i]):
-                    # if current order is greatest value .. continue
+                if ascend > descend:
                     continue
                 else:
-                    # else swap the values ..!!!
-                    lst[i] , lst[j] = lst[j] , lst[i]
-        
-        
-        ans = ''.join(lst)
-
-        if int(ans) == 0:
-            return "0"
-        
-        return ans
-
+                    str_list[sLw],str_list[fAs]=str_list[fAs],str_list[sLw]
+        Number= "".join(str_list)
+        if int(Number)==0:
+            return '0'
+        return Number
         
