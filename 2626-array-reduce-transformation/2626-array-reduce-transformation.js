35a79554aaa4cebbf653d574651ea61c23fba59b
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-14 04:40:16 -0500

    Time: 47 ms (88.37%), Space: 50 MB (12.85%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let ans = init;
+    for (let i of nums)
+        ans = fn(ans, i);
+    
+    return ans;
+    
+};
