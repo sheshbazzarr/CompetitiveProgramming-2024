7e106c919fc9744dc5d5bee17c992b8ca0970b89
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-17 05:33:46 -0500

    Time: 63 ms (61.29%), Space: 49.9 MB (66.55%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -2,20 +2,10 @@
  * @param {Function[]} functions
  * @return {Function}
  */
-var compose = function(functions) {
-    
-    if (functions.length === 0) {
-    return function(x) { return x; };
-  }
+var compose = function (functions) {
 
-  return functions.reduceRight(function(prevFn, nextFn) {
-    return function(x) {
-      return nextFn(prevFn(x));
-    };
-  });
-
+    return x => functions.reduceRight((acc,f)=>f(acc),x)
 };
-
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
