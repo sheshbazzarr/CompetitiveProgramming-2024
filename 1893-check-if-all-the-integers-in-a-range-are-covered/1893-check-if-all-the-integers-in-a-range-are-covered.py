1ef2deabd4f771ac324cc14049f8d2d9cbb57c8a
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-24 04:18:55 -0500

    Time: 38 ms (89.68%), Space: 16.5 MB (94.53%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,17 +1,15 @@
 class Solution:
     def isCovered(self, ranges, left: int, right: int):
-        covered = set()
-        for i, j in ranges:
-            for k in range(i, j + 1):
-                covered.add(k)
-        
-        for i in range(left, right+1):
-            if i not in covered:
+        inclusive=set()  
+        for  i,j in ranges:
+            for k in range(i,j+1):
+                inclusive.add(k)    
+        for i in range(left,right+1):
+            if i not in inclusive:
                 return False
-        
         return True
 
+        
 
-# inputs = input().split()  # This splits the input string into a list of strings
-# print(inputs)
+
         
