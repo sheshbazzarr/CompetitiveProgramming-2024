3987f0890759b80d002b5ea0722870e5c41ec131
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-25 01:31:58 -0500

    Time: 284 ms (97.78%), Space: 17.4 MB (28.89%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -1,7 +1,9 @@
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
-        dp = [0] * 128
+        ideal_lengths = [0] * 128
         for char in s:
             ascii_value = ord(char)
-            dp[ascii_value] = max(dp[ascii_value - k : ascii_value + k + 1]) + 1
-        return max(dp)
+            current_ideal_length = max(ideal_lengths[ascii_value - k : ascii_value + k + 1]) + 1
+            ideal_lengths[ascii_value] = current_ideal_length
+        max_ideal_length = max(ideal_lengths)
+        return max_ideal_length
