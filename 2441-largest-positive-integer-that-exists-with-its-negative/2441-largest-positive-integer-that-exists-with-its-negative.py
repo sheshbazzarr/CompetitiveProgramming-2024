ad26b0a7dba5ec1259cf480fc447ddb62adf721e
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-05-02 02:42:53 -0500

    Time: 97 ms (95.90%), Space: 16.6 MB (98.70%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
@@ -1,18 +1,14 @@
 class Solution:
     def findMaxK(self, nums: List[int]) -> int:
-         # [1] two use two-pointers approach, we need
-        #     to prepare the list by sorting
         nums.sort()
-        
-        # [2] move pointers towards each other to find
-        #     largest positive integer with its negative
-        i, j = 0, len(nums)-1
-        while i < j:
-            if nums[i] == - nums[j]:
+        i=0
+        j=len(nums)-1
+        while i<j:
+            if nums[i]==-nums[j]:
                 return nums[j]
-            if abs(nums[i]) > abs(nums[j]):
-                i += 1
+            elif abs(nums[i])> abs(nums[j]):
+                i+=1
             else:
-                j -= 1
-                
+                j-=1
         return -1
+                
