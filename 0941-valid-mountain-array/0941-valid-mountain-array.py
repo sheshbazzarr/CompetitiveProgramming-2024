b1c4b95b020c97ca9982f022728f4bfb44267be3
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-06-30 13:57:36 -0500

    Time: 155 ms (70.29%), Space: 18.2 MB (13.28%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -1,10 +1,14 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        A=0
-        B=len(arr)-1
-        D=len(arr)
-        while A+1<D and arr[A]<arr[A+1]:
+        # two person
+        A=0   # one end 
+        B=len(arr)-1 # the other side
+        D=len(arr) # distance between them 
+        while A+1<D and arr[A]<arr[A+1]: # first person climb the mountain 
             A+=1 
-        while B>0 and arr[B]<arr[B-1]:
+        while B>0 and arr[B]<arr[B-1]: # second person climb the mountain 
             B-=1
-        return 0<A==B<D-1
+        return 0<A==B<D-1 # if Person A and B Both move and meet at the pick of mountain 
+    
+    
+    # inspired by lee215
